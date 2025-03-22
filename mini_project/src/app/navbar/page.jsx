1661119d3e3ae2d4d76272f7909bf4b3db2509a1
nirmal fageria <nirmalfageria15@gmail.com>
2025-03-23 00:23:49 +0530

    minor changes done in navbar

--- mini_project/src/app/navbar/page.jsx
+++ mini_project/src/app/navbar/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-900 text-white p-4 shadow-md">
+    <header className="bg-gray-900 text-white p-4 shadow-md border-b border-gray-700">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold text-blue-400">HMPV Virus Database</h1>
         <nav>
